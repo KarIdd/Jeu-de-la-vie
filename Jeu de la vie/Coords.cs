4da0684dc762252ec097b2f815dcfaad0a218c6a
FLATRES Johan <76738358+KarIdd@users.noreply.github.com>
2021-10-19 09:42:28 +0200

    Initial commit

---
+++ Jeu de la vie/Coords.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jeu_de_la_vie
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
